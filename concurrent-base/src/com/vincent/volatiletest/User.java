9c35e8fb15b75bfcc7bc44e4280007829085cd68
VincenttHo <252130586@qq.com>
2019-04-06 09:57:38 +0800

    v1.91

--- concurrent-base/src/com/vincent/volatiletest/User.java
+++ concurrent-base/src/com/vincent/volatiletest/User.java
@@ -1,5 +1,12 @@
 package com.vincent.volatiletest;
 
+/**
+ * 用户
+ * @ClassName: User
+ * @Description: 用户
+ * @author: VincentHo
+ * @date: 2019年3月30日 下午12:39:40
+ */
 public class User {
     private volatile  int age = 0;
 
